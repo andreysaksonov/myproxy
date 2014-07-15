6f4b8a2fb12d14018678a5962514ed1c6132fbb0
Andrey Saksonov <saksonov.andrey@gmail.com>
2014-07-15 18:41:44 +0400

    Fix 'Host' header in requests

--- src/main/java/com/zandbee/mobius/ReverseMobiusVerticle.java
+++ src/main/java/com/zandbee/mobius/ReverseMobiusVerticle.java
@@ -1,5 +1,6 @@
 package com.zandbee.mobius;
 
+import com.google.common.collect.Iterables;
 import com.google.common.net.InetAddresses;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.VoidHandler;
@@ -9,8 +10,8 @@ import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
-import java.net.Inet4Address;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -23,13 +24,13 @@ public class ReverseMobiusVerticle extends Verticle {
 
     private static final String REVERSE_MOBIUS_IP = "REVERSE_MOBIUS_IP";
     private static final String REVERSE_MOBIUS_PORT = "REVERSE_MOBIUS_PORT";
-    private static final String HOST = "host";
+    private static final String HOST_KEY = "host";
 
     private JsonObject config;
     private Logger logger;
     private Map<String, String> env;
 
-    private final List<String> hosts = new ArrayList<>();
+    private Iterator<String> targets;
 
     public void start() {
         config = container.config();
@@ -49,22 +50,27 @@ public class ReverseMobiusVerticle extends Verticle {
             error("$REVERSE_MOBIUS_PORT -> Bad Port");
         }
 
-        String inetAddress = env.getOrDefault(REVERSE_MOBIUS_IP, "127.0.0.1");
-        if (!InetAddresses.isInetAddress(inetAddress)) {
+        String address = "127.0.0.1";
+        String a = env.get(REVERSE_MOBIUS_IP);
+        if (a != null) {
+            address = a;
+        }
+        if (!InetAddresses.isInetAddress(address)) {
             error("$REVERSE_MOBIUS_IP -> Bad IP");
         }
 
         RouteMatcher routeMatcher = new RouteMatcher();
-        routeMatcher.all("/*", new ReverseProxyHandler());
+        routeMatcher.all(".+", new ReverseProxyHandler());
 
-        String host = config.getString(HOST);
+        List<String> hosts = new ArrayList<String>();
+        String host = config.getString(HOST_KEY);
         if (host != null) {
             logger.info("Proxy: " + host);
             hosts.add(host);
         } else {
             int i = 1;
             do {
-                host = config.getString(HOST + i);
+                host = config.getString(HOST_KEY + i);
                 if (host != null) {
                     logger.info("Proxy " + i + ": " + host);
                     hosts.add(host);
@@ -74,11 +80,12 @@ public class ReverseMobiusVerticle extends Verticle {
         }
 
         if (hosts.size() > 0) {
+            targets = Iterables.cycle(hosts).iterator();
             final HttpServer httpsServer = vertx.createHttpServer().requestHandler(routeMatcher);
-            httpsServer.listen(port, inetAddress);
+            httpsServer.listen(port, address);
             logger.info("Started Reverse Mobius!");
         } else {
-            error("No hosts configured! Configure backend hosts via json file passed to -conf parameter!");
+            error("No hosts configured! Configure target hosts via json file passed to -conf parameter!");
         }
     }
 
@@ -89,12 +96,18 @@ public class ReverseMobiusVerticle extends Verticle {
 
     private final class ReverseProxyHandler implements Handler<HttpServerRequest> {
 
-        private int index = 0;
+        public static final String HOST_HEADER = "Host";
 
         @Override
         public void handle(final HttpServerRequest serverRequest) {
             HttpClient httpClient = vertx.createHttpClient();
-            httpClient.setHost(getHost());
+            // httpClient.setSSL(true);
+            // httpClient.setTrustAll(true);
+            // httpClient.setPort(443);
+            final String target = targets.next();
+            httpClient.setHost(target);
+            logger.debug("Target: " + target);
+            logger.debug("URI: " + serverRequest.uri());
 
             final HttpClientRequest clientRequest = httpClient.request(serverRequest.method(), serverRequest.uri(), new Handler<HttpClientResponse>() {
                 public void handle(HttpClientResponse clientResponse) {
@@ -109,23 +122,16 @@ public class ReverseMobiusVerticle extends Verticle {
                     });
                     clientResponse.endHandler(new VoidHandler() {
                         public void handle() {
+                            serverRequest.headers().set(HOST_HEADER, target);
                             serverRequest.response().end();
                         }
                     });
                 }
             });
             clientRequest.headers().set(serverRequest.headers());
+            clientRequest.headers().set(HOST_HEADER, target);
             clientRequest.setChunked(true);
             clientRequest.end();
-        }
-
-        private String getHost() {
-            String host = hosts.get(index);
-            // logger.debug("Host: " + host);
-            if (++index == hosts.size()) {
-                index = 0;
-            }
-            return host;
         }
     }
 }
